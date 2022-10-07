0468f1de0aceac6cd559e7008bdc87cb803990b6
ggoncalvesm <ggoncalvesmg@gmail.com>
2022-10-07 09:27:25 -0300

    Testes unitários foram incluídos

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+ 
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueDataFutura() {
+        LocalDate date = LocalDate.of(2030, 01, 01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+    
+    @Test
+    public void deveRetornarFalseDataPassada() {
+        LocalDate date = LocalDate.of(2010, 01, 01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+    
+    @Test
+    public void deveRetornarTrueDataAtual() {
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
